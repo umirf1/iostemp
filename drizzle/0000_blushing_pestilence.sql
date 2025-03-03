13e205d785a160c59cf7392c40f3eae873edf32e
Ansh <anshnanda10@gmail.com>
2025-03-03 17:20:01 -0500

    setup migration

---
+++ drizzle/0000_blushing_pestilence.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
